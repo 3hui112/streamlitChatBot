3edb8df59b6b29db6bf6051bc0b54a9f809b0bd0
3hui112 <shinetheworld112@gmail.com>
2024-03-26 21:44:34 +0900

    Customizing Log Formatting by modifying hello_logging.py

--- hello_logging.py
+++ hello_logging.py
@@ -1,7 +1,11 @@
 import logging
 
+#Create a custom logger
+logger = loggin.getLogger(__name__)
+
 # Configure logging
-logging.basicConfig(filename="app.log", filemode="w", format="%(asctime)s - %(message)s", level=logging.INFO)
+logging.basicConfig(filename="app.log", filemode="w", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 # Log a message
-logging.info("Hello, logging world! Logged to file.")
+logger.info("Hello, logging world! Custom format.")
+
